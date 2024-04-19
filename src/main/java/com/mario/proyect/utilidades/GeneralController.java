ed8d3fb9dca527aa35445dc45d48406d916f9c3b
maribalo <maribalo043@gmail.com>
2024-04-19 13:48:59 +0200

    GereralController medio terminado, faltarian las estadisticas entre otras cosas, además barra de nevagacion actualizada pero no terminada.

--- src/main/java/com/mario/proyect/utilidades/GeneralController.java
+++ src/main/java/com/mario/proyect/utilidades/GeneralController.java
@@ -8,12 +8,6 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class GeneralController {
 
-//    @GetMapping("/")
-//    public ModelAndView getInicio() {
-//
-//        ModelAndView model = new ModelAndView();
-//        return model;
-//    }
     @GetMapping("/torneo")
     public ModelAndView getTorneo() {
     	ModelAndView model = new ModelAndView();
@@ -21,5 +15,46 @@ public class GeneralController {
         return model;
     }
     
+    @GetMapping("/nuestrosEquipos")
+    public ModelAndView getEquipos() {
+    	ModelAndView model = new ModelAndView();
+    	model.setViewName("generalHTML/NuestrosEquipos");
+        return model;
+    }
+    
+    @GetMapping("/sobreNosotros")
+    public ModelAndView getSobreNosotros() {
+    	ModelAndView model = new ModelAndView();
+    	model.setViewName("generalHTML/SobreNosotros");
+        return model;
+    }
+    
+    @GetMapping("/contacto")
+    public ModelAndView getContacto() {
+    	ModelAndView model = new ModelAndView();
+    	model.setViewName("generalHTML/Contacto");
+        return model;
+    }
+    
+    @GetMapping("/links")
+    public ModelAndView getLinks() {
+    	ModelAndView model = new ModelAndView();
+    	model.setViewName("generalHTML/Links");
+        return model;
+    }
+    
+    @GetMapping("/avisoLegal")
+    public ModelAndView getAvisoLegal() {
+    	ModelAndView model = new ModelAndView();
+    	model.setViewName("generalHTML/AvisoLegal");
+        return model;
+    }
+    
+    @GetMapping("/politicaPrivacidad")
+    public ModelAndView getPoliticaDePrivacidad() {
+    	ModelAndView model = new ModelAndView();
+    	model.setViewName("generalHTML/PoliticaPrivacidad");
+        return model;
+    }
     
 }
