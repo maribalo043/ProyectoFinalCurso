02d930fde96f1164f8b358c038d127eaf9976ff9
Mario <mariobl90@educastur.es>
2024-05-19 21:11:28 +0200

    Ver los partidos del torneo en función de la categoria terminado

--- src/main/java/com/mario/proyect/categoria/CategoriaDAO.java
+++ src/main/java/com/mario/proyect/categoria/CategoriaDAO.java
@@ -4,10 +4,21 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.mario.proyect.partido.Partido;
 
 public interface CategoriaDAO extends CrudRepository<Categoria,Long>{
 
     @Query(value = "SELECT * FROM categorias WHERE activa=true",nativeQuery = true)
     List<Categoria> categoriasActive();
+
+    @Query("SELECT p FROM Partido p " +
+    "JOIN Equipo eqLocal ON p.equipoLocal.id = eqLocal.id " +
+    "JOIN Categoria c ON eqLocal.categoria.id = c.id " +
+    "WHERE c.nombre = :nombreCategoria")
+List<Partido> findPartidosByNombreCategoria(@Param("nombreCategoria") String nombreCategoria);
+
+
     
 }
--- src/main/java/com/mario/proyect/utilidades/GeneralController.java
+++ src/main/java/com/mario/proyect/utilidades/GeneralController.java
@@ -1,12 +1,27 @@
 package com.mario.proyect.utilidades;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.mario.proyect.categoria.CategoriaDAO;
+import com.mario.proyect.partido.Partido;
+import com.mario.proyect.partido.PartidoDAO;
+
 
 
 @Controller
 public class GeneralController {
+
+    @Autowired 
+    private CategoriaDAO categoriaDao;
+
+    @Autowired 
+    private PartidoDAO partidoDao;
 
     @GetMapping("/torneo")
     public ModelAndView getTorneo() {
@@ -47,6 +62,27 @@ public class GeneralController {
     public ModelAndView getPoliticaDePrivacidad() {
     	ModelAndView model = new ModelAndView();
     	model.setViewName("generalHTML/PoliticaPrivacidad");
+        return model;
+    }
+
+    @GetMapping("/torneo/categorias")
+    public ModelAndView getCategorias() {
+        ModelAndView model = new ModelAndView();
+        model.setViewName("/torneoHTML/SeleccionCategoria");
+
+        model.addObject("categorias", categoriaDao.categoriasActive());
+        return model;
+    }
+
+    
+    @GetMapping("/torneo/categoria/{cat}")
+    public ModelAndView getPartidosCategoria(@PathVariable String cat) {
+        ModelAndView model = new ModelAndView();
+        List<Partido> partidos = categoriaDao.findPartidosByNombreCategoria(cat);
+
+        model.setViewName("/torneoHTML/PartidosCategoria");
+        model.addObject("partidos", partidos);
+
         return model;
     }
 
